69bd49027706206fc88cefdea247b45f5dd246b1
lybecker <anders@lybecker.com>
2017-03-01 17:20:11 +0100

    Initial Windows 10 IoT client with TPM

---
+++ WindowsIoTCore/WindowsIoTCore/MainPage.xaml.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using System.Text;
+using Microsoft.Azure.Devices.Client;
+using Microsoft.Devices.Tpm;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using Windows.System.Threading;
+using System.Threading;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace WindowsIoTCore
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MyViewModel _viewModel { get; set; }
+        private DispatcherTimer _timer;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            this._viewModel = new MyViewModel();
+
+            _viewModel.PropertyChanged += ViewModel_PropertyChanged;
+            
+            SetupTimer();
+            StartStopTimer(_viewModel);
+        }
+
+        private void ViewModel_PropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            var viewModel = (MyViewModel)sender;
+
+            switch (e.PropertyName)
+            {
+                case "AutomaticSendingMessages":
+                    StartStopTimer(viewModel);
+                    break;
+            }
+        }
+
+        void SetupTimer()
+        {
+            TimeSpan delay = TimeSpan.FromSeconds(1);
+
+            _timer = new DispatcherTimer();
+            _timer.Interval = delay;
+            _timer.Tick += (object sender, object e) =>
+                {
+                    SendDeviceToCloudMessagesAsync();
+                };
+        }
+
+        void StartStopTimer(MyViewModel viewModel)
+        {
+            if (viewModel.AutomaticSendingMessages)
+                _timer.Start();
+            else
+                _timer.Stop();
+        }
+
+        async Task SendDeviceToCloudMessagesAsync()  
+        {
+            TpmDevice device = new TpmDevice(0); // Use logical device 0 on the TPM by default
+
+            string iotHubUri = device.GetHostName();
+            string deviceId = device.GetDeviceId();
+            string sasToken = device.GetSASToken(validity: 3600);
+
+            var deviceClient = DeviceClient.Create(
+                iotHubUri,
+                AuthenticationMethodFactory.CreateAuthenticationWithToken(deviceId, sasToken), TransportType.Http1);
+
+            var str = _viewModel.Message;
+            var message = new Message(Encoding.ASCII.GetBytes(str));
+
+            await deviceClient.SendEventAsync(message);
+        }
+
+        async Task<string> ReceiveCloudToDeviceMessageAsync()
+        {
+            TpmDevice myDevice = new TpmDevice(0); // Use logical device 0 on the TPM by default
+            string hubUri = myDevice.GetHostName();
+            string deviceId = myDevice.GetDeviceId();
+            string sasToken = myDevice.GetSASToken(validity: 3600);
+
+            var deviceClient = DeviceClient.Create(
+                hubUri,
+                AuthenticationMethodFactory.
+                    CreateAuthenticationWithToken(deviceId, sasToken), TransportType.Http1);
+
+            while (true)
+            {
+                var receivedMessage = await deviceClient.ReceiveAsync();
+
+                if (receivedMessage != null)
+                {
+                    var messageData = Encoding.ASCII.GetString(receivedMessage.GetBytes());
+                    await deviceClient.CompleteAsync(receivedMessage);
+                    return messageData;
+                }
+
+                await Task.Delay(TimeSpan.FromSeconds(1));
+            }
+        }
+
+        private async void SendMessage_Click(object sender, RoutedEventArgs e)
+        {
+            await SendDeviceToCloudMessagesAsync();
+        }
+    }
+}
---
+++ WindowsIoTCore/WindowsIoTCore/MyViewModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace WindowsIoTCore
+{
+    public class MyViewModel : INotifyPropertyChanged
+    {
+        public MyViewModel()
+        {
+            _automaticSendingMessages = true;
+            _message = "Hello, Cloud from Windows 10 IoT";
+        }
+
+        private bool _automaticSendingMessages;
+        public bool AutomaticSendingMessages
+        {
+            get { return _automaticSendingMessages; }
+            set
+            {
+                _automaticSendingMessages = value;
+                this.OnPropertyChanged();
+            }
+        }
+
+        string _message;
+        public string Message
+        {
+            get { return _message; }
+            set
+            {
+                _message = value;
+                this.OnPropertyChanged();
+            }
+        }
+        
+        public void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            // Raise the PropertyChanged event, passing the name of the property whose value has changed.
+            this.PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void Do()
+        {
+
+
+        }
+    }
+}
