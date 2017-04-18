6b8152294a4383aaf5d96de54b38d6e1170cc81b
lybecker <anders@lybecker.com>
2017-04-18 15:25:40 +0200

    initial version of the dotnet console for consuming TI SensorTag temperature readings

---
+++ TiSensorTagIoTHubConsole/TiSensorTagIoTHubConsole/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.ServiceBus.Messaging;
+using System.Threading;
+
+namespace TiSensorTagIoTHubConsole
+{
+    class Program
+    {
+        static string connectionString = "<Azure IoT Hub connectionstring>";
+        static string iotHubD2cEndpoint = "messages/events";
+        static EventHubClient eventHubClient;
+        static TemperatureScale temperatureScale = TemperatureScale.Celsius;
+        static TiSensorTagTemperatureCalculator calculator = new TiSensorTagTemperatureCalculator();
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Receive messages. Ctrl-C to exit.\n");
+            eventHubClient = EventHubClient.CreateFromConnectionString(connectionString, iotHubD2cEndpoint);
+
+            var d2cPartitions = eventHubClient.GetRuntimeInformation().PartitionIds;
+
+            CancellationTokenSource cts = new CancellationTokenSource();
+
+            System.Console.CancelKeyPress += (s, e) =>
+            {
+                e.Cancel = true;
+                cts.Cancel();
+                Console.WriteLine("Exiting...");
+            };
+
+            var tasks = new List<Task>();
+            foreach (string partition in d2cPartitions)
+            {
+                tasks.Add(ReceiveMessagesFromDeviceAsync(partition, cts.Token));
+            }
+            Task.WaitAll(tasks.ToArray());
+        }
+
+        private static async Task ReceiveMessagesFromDeviceAsync(string partition, CancellationToken ct)
+        {
+            var eventHubReceiver = eventHubClient.GetDefaultConsumerGroup().CreateReceiver(partition, DateTime.UtcNow);
+            while (true)
+            {
+                if (ct.IsCancellationRequested) break;
+
+                EventData eventData = await eventHubReceiver.ReceiveAsync();
+
+                if (eventData == null) continue;
+
+                var sensorData = eventData.GetBytes();
+
+                // this is how to calculate the temperature according to the TI docs
+                //var ambientTemp = calculator.CalculateAmbientTemperature(sensorData, temperatureScale);
+                //var targetTemp = calculator.CalculateTargetTemperature(sensorData, ambientTemp, temperatureScale);
+
+                //Console.WriteLine($"Partion {partition} - {targetTemp}, {ambientTemp}");
+
+                // this is how the Azure IoT Hub Gateway module ble_printer does it 
+                double ambient = 0, target = 0;
+
+                sensortag_temp_convert(BitConverter.ToUInt16(sensorData, 2), BitConverter.ToUInt16(sensorData, 0), ref ambient, ref target);
+
+                Console.WriteLine($"Partion {partition} - {target}, {ambient}");
+            }
+        }
+
+        // https://github.com/Azure/azure-iot-gateway-sdk/blob/master/samples/ble_gateway/ble_printer/src/ble_printer.c
+        static void sensortag_temp_convert(UInt16 rawAmbTemp, UInt16 rawObjTemp, ref double tAmb, ref double tObj)
+        {
+            const double SCALE_LSB = 0.03125;
+            double t;
+            int it;
+
+            it = (int)((rawObjTemp) >> 2);
+            t = ((double)(it)) * SCALE_LSB;
+            tObj = t;
+
+            it = (int)((rawAmbTemp) >> 2);
+            t = (double)it;
+            tAmb = t * SCALE_LSB;
+        }
+    }
+}
---
+++ TiSensorTagIoTHubConsole/TiSensorTagIoTHubConsole/TiSensorTagTemperatureCalculator.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace TiSensorTagIoTHubConsole
+{
+    // http://processors.wiki.ti.com/index.php/SensorTag_User_Guide
+    public class TiSensorTagTemperatureCalculator
+    {
+        public double CalculateAmbientTemperature(byte[] sensorData, TemperatureScale scale)
+        {
+            int offset = 2;
+
+            if (scale == TemperatureScale.Celsius)
+                return BitConverter.ToUInt16(sensorData, offset) / 128.0;
+            else
+                return (BitConverter.ToUInt16(sensorData, offset) / 128.0) * 1.8 + 32;
+        }
+
+        public double CalculateTargetTemperature(byte[] sensorData, double ambientTemperature, TemperatureScale scale)
+        {
+            if (scale == TemperatureScale.Celsius)
+                return CalculateTargetTemperature(sensorData, ambientTemperature);
+            else
+                return CalculateTargetTemperature(sensorData, ambientTemperature) * 1.8 + 32;
+        }
+
+
+
+        // info about the calculation: http://www.ti.com/lit/ug/sbou107/sbou107.pdf
+        private double CalculateTargetTemperature(byte[] sensorData, double ambientTemperature)
+        {
+            double Vobj2 = BitConverter.ToInt16(sensorData, 0);
+            Vobj2 *= 0.00000015625;
+
+            double Tdie = ambientTemperature + 273.15;
+
+            double S0 = 5.593E-14;  // Calibration factor
+            double a1 = 1.75E-3;
+            double a2 = -1.678E-5;
+            double b0 = -2.94E-5;
+            double b1 = -5.7E-7;
+            double b2 = 4.63E-9;
+            double c2 = 13.4;
+            double Tref = 298.15;
+            double S = S0 * (1 + a1 * (Tdie - Tref) + a2 * Math.Pow((Tdie - Tref), 2));
+            double Vos = b0 + b1 * (Tdie - Tref) + b2 * Math.Pow((Tdie - Tref), 2);
+            double fObj = (Vobj2 - Vos) + c2 * Math.Pow((Vobj2 - Vos), 2);
+            double tObj = Math.Pow(Math.Pow(Tdie, 4) + (fObj / S), .25);
+
+            return tObj - 273.15;
+        }
+    }
+
+    public enum TemperatureScale
+    {
+        Celsius,
+        Farenheit
+    }
+}
